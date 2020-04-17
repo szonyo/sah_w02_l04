4ef4a5c67cb59ced863a1e15095075a32f621e42
szonyo <63702203+szonyo@users.noreply.github.com>
2020-04-17 07:52:39 +0200

    Added CSS and JS practice

---
+++ script.js
@@ -0,0 +1,36 @@
+let name = 'Szonja';
+console.log(`Hello my dear ${name}`);
+
+// jquery part
+
+$('p').html('<strong>The jQuery</strong> part has started!');
+$('h1').css('background','blue');
+
+$('h2').toggleClass('green');
+
+$('main').append ('<p id="first">Added element</p>');
+$('main').append ('<p id="second">Added second element</p>');
+
+
+$('#add-button').click(() => {
+    if ($('main p').length < 5) {
+    $('main').append('<p>Added a new p tag</p>');
+} else {
+    $('main').append('<p class="green">Added a new p tag</p>');
+});
+
+let removeFunction = () => {
+    $('main p').remove ();
+}
+
+$('#remove-button').click(removeFunction);
+
+//Last part: conditions and loops
+
+let age=29;
+
+if ( age >= 18) {
+    console.log('This person can drink')
+}else{
+    console.log('This is a kid, no alcohol.')
+}
