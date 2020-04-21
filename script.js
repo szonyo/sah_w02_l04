373a222aae57a3631b8b06fd70e63c78d3537849
szonyo <63702203+szonyo@users.noreply.github.com>
2020-04-21 18:07:26 +0200

    added conditions

--- script.js
+++ script.js
@@ -3,34 +3,38 @@ console.log(`Hello my dear ${name}`);
 
 // jquery part
 
-$('p').html('<strong>The jQuery</strong> part has started!');
+$('h2').html('<strong>The jQuery</strong> part has started!');
 $('h1').css('background','blue');
 
 $('h2').toggleClass('green');
+$('p').toggleClass('green');
+$('p').toggleClass('green');
 
-$('main').append ('<p id="first">Added element</p>');
-$('main').append ('<p id="second">Added second element</p>');
+// $('main').append ('<p id="first">Added element</p>');
+// $('main').append ('<p id="second">Added second element</p>');
 
+$('#first').remove();
 
 $('#add-button').click(() => {
-    if ($('main p').length < 5) {
+    if($('main p'.length > 5)){
     $('main').append('<p>Added a new p tag</p>');
-} else {
+} else { 
     $('main').append('<p class="green">Added a new p tag</p>');
+}
 });
 
 let removeFunction = () => {
-    $('main p').remove ();
+    $('main p:last-child').remove ();
 }
 
 $('#remove-button').click(removeFunction);
 
 //Last part: conditions and loops
+ 
+let age = 29;
 
-let age=29;
-
-if ( age >= 18) {
-    console.log('This person can drink')
-}else{
-    console.log('This is a kid, no alcohol.')
+if (age>=18) {
+    console.log('This person can drik.')
+} else {
+console.log ('This is a kid, no alcohol.')
 }
